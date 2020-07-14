d05accda5de6249c3b0fe13c78779d9e4990fa1c
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2020-07-14 13:47:21 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+var time = new Date();
+var greeting;
+
+
+
+if ( (time.getDate() !==6 && time.getDate() !==9 && time.getDate() !==18 && time.getDate() !==26  && time.getDate() !==31 )) {
+ greeting=document.getElementById('a').style.display='block';
+} else {
+
+greeting= document.getElementById('a').style.display='none';
+}
+
+
+document.image = greeting;
+
+
+
+
+
+
