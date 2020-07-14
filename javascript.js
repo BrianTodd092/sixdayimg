608dc59101fa742251722421e273caedf38c65f3
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2020-07-14 13:54:37 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,10 +4,10 @@ var greeting;
 
 
 if ( (time.getDate() !==6 && time.getDate() !==9 && time.getDate() !==18 && time.getDate() !==26  && time.getDate() !==31 )) {
- greeting=document.getElementById('a').style.display='block';
+ greeting=document.getElementById('a').style.display='none';
 } else {
 
-greeting= document.getElementById('a').style.display='none';
+greeting= document.getElementById('a').style.display='block';
 }
 
 
