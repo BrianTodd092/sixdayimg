2e8fad7be1c29168911251a64886716b1a116d2f
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2020-07-14 14:03:47 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 var time = new Date();
 var greeting;
 
-
+document.getElementById('a').style.display='none';
 
 if ( (time.getDate() !==6 && time.getDate() !==9 && time.getDate() !==18 && time.getDate() !==26  && time.getDate() !==31 )) {
  greeting=document.getElementById('a').style.display='none';
